714bbdcc54ff76ec5407dd417a232bd85ae600f6
root <root@f18321c9-d4ef-0310-95bf-8b093ce707b5>
2004-06-25 04:02:52 +0000

    - fixed remove(GraphElement ge) method to do some extra bookkeeping (i.e.   making sure that all pointers to removed elements are removed from other   graph elements.

--- nv2d/trunk/src/nv2d/graph/directed/DGraph.java
+++ nv2d/trunk/src/nv2d/graph/directed/DGraph.java
@@ -1,7 +1,8 @@
 package nv2d.graph.directed;
 
-import java.util.Set;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 import java.lang.IllegalArgumentException;
 
 import cern.colt.matrix.impl.SparseDoubleMatrix2D;
@@ -62,16 +63,52 @@ public class DGraph extends Graph {
 		return null;
 	}
 
+	/** Remove a graph element from the graph. */
 	public boolean remove(GraphElement ge) {
 		if(ge.getClass() == DVertex.class) {
-			return _v.remove(ge);
+			_e.removeAll(((DVertex) ge).inEdges());
+			_e.removeAll(((DVertex) ge).outEdges());
+			_v.remove(ge);
+			cleanupVertex(((DVertex) ge).neighbors());
+			/* This return value is meaningless for now */
+			return true;
 		}
 		if(ge.getClass() == DEdge.class) {
-			return _e.remove(ge);
+			_e.remove(ge);
+			cleanupVertex((DVertex) ((DEdge) ge).getSource());
+			cleanupVertex((DVertex) ((DEdge) ge).getDest());
+			/* This return value is meaningless for now */
+			return true;
 		}
 
 		/* TODO: need to add cases for other types here */
 		throw new IllegalArgumentException("You must add a Directed graph element to a Directed Graph.");
+	}
+
+	/* The remove operation sometimes leave vertices with pointers to edges
+	 * that no longer exist.  This method should be called to clean them up. */
+	private void cleanupVertex(Set vertices) {
+		Iterator i = vertices.iterator();
+		while(i.hasNext()) {
+			cleanupVertex((DVertex) i.next());
+		}
+	}
+
+	private void cleanupVertex(DVertex v) {
+		Iterator j = v.inEdges().iterator();
+		while(j.hasNext()) {
+			DEdge e = (DEdge) j.next();
+			if(!_e.contains(e)) {
+				v.removeEdge(e);
+			}
+		}
+		j = v.outEdges().iterator();
+		while(j.hasNext()) {
+			DEdge e = (DEdge) j.next();
+			if(!_e.contains(e)) {
+				v.removeEdge(e);
+			}
+		}
 	}
 
 	public void clear() {
