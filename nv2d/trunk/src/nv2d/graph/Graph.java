bc78fe3569e2648553b8be3692eb354f76e3f758
root <root@f18321c9-d4ef-0310-95bf-8b093ce707b5>
2004-06-26 22:55:57 +0000

    - Added JavaDoc comments.

--- nv2d/trunk/src/nv2d/graph/Graph.java
+++ nv2d/trunk/src/nv2d/graph/Graph.java
@@ -5,14 +5,33 @@ import java.util.Set;
 import cern.colt.Sorting;
 
 public abstract class Graph extends DataStore {
+	/* Accessors */
+	/** Get a set of the edges in the graph. */
 	public abstract Set getEdges();
+
+	/** Get a set of the vertices in the graph. */
 	public abstract Set getVertices();
+	
+	/** Returns the number of unique vertices in the graph. */
 	public abstract int numVertices();
+
+	/** Returns the number of unique edges in the graph. */
 	public abstract int numEdges();
 
+	/** Returns a subset of the graph containing only the vertices in the
+	 * parameter Set. */
 	public abstract Graph subset(Set vertices);
 
+	/* Modifiers */
+	/** Add a <code>GraphElement</code> to the graph.  Note that unless there
+	 * is a class which knows how to render the graph element, it will not be
+	 * shown. */
 	public abstract boolean add(GraphElement ge);
+
+	/** Remove a <code>GraphElement</code> from the graph. */
 	public abstract boolean remove(GraphElement ge);
+
+	/** Clear all the data contained in this graph.  This includes objects of
+	 * the type <code>Datum</code> and <code>GraphElement</code>. */
 	public abstract void clear();
 }
