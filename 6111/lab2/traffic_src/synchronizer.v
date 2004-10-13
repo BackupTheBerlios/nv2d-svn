b9dbd9324018e96a13b03663dc52111314be5c9c
bshi <bshi@f18321c9-d4ef-0310-95bf-8b093ce707b5>
2004-10-13 01:12:10 +0000

    - synchronizer syncs sensor input

--- 6111/lab2/traffic_src/synchronizer.v
+++ 6111/lab2/traffic_src/synchronizer.v
@@ -5,10 +5,17 @@ module synchronizer(
 	input clk, reset, sensor, walk, reprog;
 	output s_reset, s_sensor, s_wr, s_reprog;
 
+	reg rs1, rs2;
+
 	exsync i_reset (clk, reset, s_reset);
 	// exsync i_sensor(clk, sensor, s_sensor);
 	exsync i_wr (clk, walk, s_wr);
 	exsync i_reprog (clk, reprog, s_reprog);
 
-	assign s_sensor = sensor;
+	// synchronize sensor input
+	always @ (posedge clk) begin
+		rs1 <= sensor;
+		rs2 <= rs1;
+	end
+	assign s_sensor = rs2;
 endmodule
