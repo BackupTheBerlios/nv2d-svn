51968211a053ac537001008289ff0124ab2d9fa4
bshi <bshi@f18321c9-d4ef-0310-95bf-8b093ce707b5>
2005-02-20 05:42:41 +0000

    - changed default settings

--- nv2d/trunk/src/nv2d/render/RenderSettings.java
+++ nv2d/trunk/src/nv2d/render/RenderSettings.java
@@ -11,11 +11,11 @@ public class RenderSettings {
 	/* uniqueID, description, setting */
 	static String [][] _settings = {
 		{/*antialias*/ TYPE_BOOLEAN, "t", "Anti-alias rendering."},
-		{/*show_arrows*/ TYPE_BOOLEAN, "t", "Show direction arrows on edges."},
-		{/*show_einfo*/ TYPE_BOOLEAN, "t", "Show selected edge information and statistics."},
-		{/*show_vinfo*/ TYPE_BOOLEAN, "t", "Show selected vertex information and statistics."},
-		{/*show_ginfo*/ TYPE_BOOLEAN, "t", "Show graph information and statistics."},
-		{/*show_ginfo*/ TYPE_BOOLEAN, "t", "Show graph information and statistics."}
+		{/*show_arrows*/ TYPE_BOOLEAN, "t", "Show labels for each vertex."},
+		{/*show_einfo*/ TYPE_BOOLEAN, "f", "Show stress value for each edge."},
+		{/*show_vinfo*/ TYPE_BOOLEAN, "f", "Show edge length next to each edge."},
+		{/*show_ginfo*/ TYPE_BOOLEAN, "f", "Show graph information and statistics."},
+		{/*show_ginfo*/ TYPE_BOOLEAN, "f", "Show graph vertices measures."}
 		//{/**/, "", ""}
 		//{/**/, "", ""}
 	};
