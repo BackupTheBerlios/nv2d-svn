f914e01bd96e177833098a8d699d4dad637d4e41
bshi <bshi@f18321c9-d4ef-0310-95bf-8b093ce707b5>
2005-02-28 03:02:23 +0000

    - GNU License note at the beginning of source files.

--- nv2d/trunk/src/nv2d/utils/JarListing.java
+++ nv2d/trunk/src/nv2d/utils/JarListing.java
@@ -1,3 +1,22 @@
+/**
+ * NV2D - Social Network Visualization
+ * Copyright (C) 2005 Bo Shi
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 package nv2d.utils;
 
 import java.io.File;
@@ -16,31 +35,31 @@ import java.util.jar.Manifest;
 
 public class JarListing {
 	/* Testing */
-
+	
 	public static Enumeration getListing(URL url) {
 		try {
 			// Get the jar file
 			JarURLConnection conn = (JarURLConnection) url.openConnection();
 			JarFile jar = conn.getJarFile();
-
+			
 			return jar.entries();
 		} catch (IOException e) {
 			System.err.println(e);
 			return null;
 		}
 	}
-
+	
 	public static Enumeration getListing(File f) {
 		try {
 			JarFile jar = new JarFile(f);
-
+			
 			return jar.entries();
 		} catch (IOException e) {
 			System.err.println(e);
 			return null;
 		}
 	}
-
+	
 	/** Get a listing of the directories of plugins in the directory
 	 * indicated by <code>filter</code>. */
 	public static Enumeration getPluginListing(String url, String filter) {
@@ -65,12 +84,4 @@ public class JarListing {
 			return null;
 		}
 	}
-
-	/* // for testing only
-	public static void main(String [] args) {
-		for(Enumeration e = getPluginListing("jar:http://web.mit.edu/bshi/www/N2.jar!/", "nv2d/plugins/standard"); e.hasMoreElements();) {
-			System.out.println(e.nextElement());
-		}
-	}
-	*/
 }
