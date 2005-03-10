03ad231b468aec1a2c06f5971dc29ff77f6a4a35
bshi <bshi@f18321c9-d4ef-0310-95bf-8b093ce707b5>
2005-03-10 21:13:21 +0000

    - cleanup

--- nv2d/trunk/src/nv2d/plugins/NV2DPlugin.java
+++ nv2d/trunk/src/nv2d/plugins/NV2DPlugin.java
@@ -37,21 +37,10 @@ public interface NV2DPlugin {
 	 * */
 	public void initialize(Graph g, Container view, NController control);
 
-	/** This method is called periodically by the plugin manager.  If you need
-	 * to schedule actions, use this method.
-	 * */
-	public void heartbeat();
-
 	/** If this plugin has set any DATUM's, this method should clean them up
 	 * here.
 	 * */
 	public void cleanup();
-
-	/** Return a handle to the user interface for this plugin.  This method may
-	 * return null, in which case the plugin is indicating that it does not
-	 * require a graphical user interface.
-	 * */
-	public JPanel ui();
 
 	/** Return a handle to the menu for this plugin.  The menu returned will be
 	 * accessible under "Plugins"-->"Plugin Name".  This method may return
