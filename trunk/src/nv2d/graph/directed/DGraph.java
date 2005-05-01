c60094480c11f61d15e0dfa3504d8391b7fd8f28
bshi <bshi@f18321c9-d4ef-0310-95bf-8b093ce707b5>
2005-05-01 01:21:40 +0000

    - added Id property tag to all java source files in the src/ directory - added license notice to new files

--- trunk/src/nv2d/graph/directed/DGraph.java
+++ trunk/src/nv2d/graph/directed/DGraph.java
@@ -1,6 +1,7 @@
 /**
  * NV2D - Social Network Visualization
  * Copyright (C) 2005 Bo Shi
+ * $Id$
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
--- trunk/src/nv2d/render/LegendColorizer.java
+++ trunk/src/nv2d/render/LegendColorizer.java
@@ -1,7 +1,21 @@
-/*
- * LegendColorizer.java
- *
- * Created on March 18, 2005, 4:15 PM
+/**
+ * NV2D - Social Network Visualization
+ * Copyright (C) 2005 Bo Shi
+ * $Id$
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
 package nv2d.render;
